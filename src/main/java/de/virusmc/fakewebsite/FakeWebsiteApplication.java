8f035c0f750c9909d38007d30546121ffdbdf424
VirusMc <virus.mc.tlstudios@gmail.com>
2022-06-20 09:29:46 +0200

    Spring Boot hinzugefügt mit Spring Initializr
    Signed-off-by: VirusMc <virus.mc.tlstudios@gmail.com>

---
+++ src/main/java/de/virusmc/fakewebsite/FakeWebsiteApplication.java
@@ -0,0 +1,13 @@
+package de.virusmc.fakewebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/virusmc/fakewebsite/FakeWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package de.virusmc.fakewebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
