fff356017c2657cb013f27ea68cf57e9c3b6d2a4
VirusMc <virus.mc.tlstudios@gmail.com>
2022-06-20 09:56:16 +0200

    Flogger hinzugefügt; application.properties zu .yaml; doStartupStuff hinzugefügt;
    Signed-off-by: VirusMc <virus.mc.tlstudios@gmail.com>

--- src/main/java/de/virusmc/fakewebsite/FakeWebsiteApplication.java
+++ src/main/java/de/virusmc/fakewebsite/FakeWebsiteApplication.java
@@ -1,13 +1,23 @@
 package de.virusmc.fakewebsite;
 
+import lombok.extern.flogger.Flogger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.yaml")
+@Flogger
 public class FakeWebsiteApplication {
 
+	public static final Class<FakeWebsiteApplication> APPLICATION_CLASS = FakeWebsiteApplication.class;
+
 	public static void main(String[] args) {
-		SpringApplication.run(FakeWebsiteApplication.class, args);
+		new SpringApplication(APPLICATION_CLASS).run(args).getBean(APPLICATION_CLASS).doStartupStuff();
+	}
+
+	public void doStartupStuff() {
+		log.atInfo().log("-- Started FakeWebsiteApplication --");
 	}
 
 }
